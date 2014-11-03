cc1a2a86f46be39d21d71acb2713e88744b997ea
YangFuchi <a14z8fy@aiit.ac.jp>
2014-11-03 15:24:20 +0000

    new

--- lib/a14z8fy_pound2kg.rb
+++ lib/a14z8fy_pound2kg.rb
@@ -1,5 +1,15 @@
 require "a14z8fy_pound2kg/version"
 
 module A14z8fyPound2kg
-  puts 'this is a gem on AIIT lecture.'
+
+class P2k
+ def toKg(lb)
+   lb * 0.4535
+ end
+end
+
+lb = P2k.new
+
+puts lb.toKg(ARGV[0].to_f)
+
 end
--- lib/a14z8fy_pound2kg/version.rb
+++ lib/a14z8fy_pound2kg/version.rb
@@ -1,3 +1,3 @@
 module A14z8fyPound2kg
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
